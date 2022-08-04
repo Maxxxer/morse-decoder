cd43ac9ac8c92db457817ea26a920499e2fc6028
Maksim <polonets.maksim@gmail.com>
2022-08-04 14:11:56 +0300

    fix: bugfix

--- src/index.js
+++ src/index.js
@@ -43,25 +43,22 @@ function decode(expr) {
     let string = expr.length % 10 == 0 ? expr : expr.padStart(expr.length + 10 - expr.length % 10, '0');
     let letters = [];
 
-    letters.push(string.match(/........../g));
-    letters = letters.flat().map(item => {
-        item.replace(/11/g, '-');
-        item.replace(/11/g, '-');
-        item.replace(/0/g, '');
-        return item;
-    }
-    );
+    letters.push(string.match(/........../g))
+    letters = letters.flat();
+    letters = letters.map(item => item.replace(/11/g, '-'));
+    letters = letters.map(item => item.replace(/10/g, '.'));
+    letters = letters.map(item => item.replace(/0/g, ''));
 
     letters = letters.map(item => {
-        for (let sign in MORSE_TABLE) {
+        for (let sign in MORSE_TABLE)
             if (sign === item)
                 item = MORSE_TABLE[sign];
-        }
         return item;
     });
     letters = letters.join('')
 
     return letters;
+
 }
 
 
